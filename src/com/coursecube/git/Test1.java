7f9b574036c027ee199089f740e8664ad6f830da
Jay <er.yadavjay@gmail.com>
2020-12-13 21:09:45 +0530

    First Commit

---
+++ src/com/coursecube/git/Test1.java
@@ -0,0 +1,22 @@
+package com.coursecube.git;
+
+import java.util.Scanner;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+	Scanner st=new Scanner(System.in);
+	 System.out.println("Enter a value -1");
+	 int a=st.nextInt();
+	 System.out.println("Enter a value -2");
+	 
+	 int b=st.nextInt();
+	 int c=a+b;
+	 System.out.println(c);
+	 
+	
+	
+	
+	}
+
+}
